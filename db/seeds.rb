41c7b35a0c22e331dbbae6ce6869846fa7b51a3c
baptiste dauphin <bap_dauphin@hotmail.fr>
2021-06-29 15:33:01 +0200

    put seeds for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create!(name: 'Zaatar', arrondissement: 11, address: '49 Boulevard Voltaire,75011, Paris, 75011', type_of_food: 'Gastronomy', description: 'le meilleur mezzé du meilleur restaurant Libanais de Paris', note: 1)
+Restaurant.create!(name: 'Surpriz', arrondissement: 11, address: '110 Rue Oberkampf, 75011 Paris', type_of_food: 'Asian', description: 'Sürpriz amène la vraie recette du Döner Kebab de Berlin.', note: 5)
+Restaurant.create!(name: 'Camion qui Fume', arrondissement: 11, address: '66 rue Oberkampf, 75011', type_of_food: 'Fast Food', description: 'Le meilleur burger à Paris 2ème cest chez Le Camion Qui Fume qui vous propose la livraison à domicile et au bureau', note: 4)
+Restaurant.create!(name: 'Mamma Roma', arrondissement: 11, address: '90 Rue Oberkampf, 75011 Paris', type_of_food: 'Italian', description: 'Une très bonne adresse, la pizza est trop bonne, differents types de pizza, des pâtes, desserts..', note: 3)
+Restaurant.create!(name: 'Le Favori', arrondissement: 11, address: '112 Rue Saint-Maur', type_of_food: 'American', description: 'Des sandwichs de grande qualité', note: 2)
